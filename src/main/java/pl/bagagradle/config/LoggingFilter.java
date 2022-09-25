adcc6be3a510bb9f676131fa41c675c27d92f1ec
barbaragawalewicz <basiekgwl@gmail.com>
2022-09-25 03:21:16 +0200

    add configurations and request/response logging

---
+++ src/main/java/pl/bagagradle/config/LoggingFilter.java
@@ -0,0 +1,49 @@
+package pl.bagagradle.config;
+
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+@Slf4j
+@Component
+public class LoggingFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper(request);
+        ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(response);
+
+        long startTime = System.currentTimeMillis();
+        filterChain.doFilter(requestWrapper, responseWrapper);
+        long timeTaken = System.currentTimeMillis() - startTime;
+
+        String requestBody = getStringValue(requestWrapper.getContentAsByteArray(), request.getCharacterEncoding());
+        String responseBody = getStringValue(responseWrapper.getContentAsByteArray(), response.getCharacterEncoding());
+
+        log.info("FINISHED PROCESSING: METHOD={}; REQUEST_URI={}; REQ_PAYLOAD={}; RESPONSE_CODE={}; RESPONSE={}; TIME_TAKEN={}",
+                request.getMethod(), request.getRequestURI(), requestBody, response.getStatus(), responseBody, timeTaken);
+        responseWrapper.copyBodyToResponse();
+    }
+
+
+    private String getStringValue(byte[] contentAsByteArray, String characterEncoding) {
+
+        try {
+            return new String(contentAsByteArray, 0, contentAsByteArray.length, characterEncoding);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+}
